bc61accff23a890ffa8d6865ac4e7d08c638e00f
morroko <girishrawat007@gmail.com>
2013-01-11 15:45:18 +0530

    second commit on my original code

---
+++ KMLViewerViewController.h
@@ -0,0 +1,15 @@
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+#import "KMLParser.h"
+
+@interface KMLViewerViewController : UIViewController {
+    IBOutlet MKMapView *map;
+    KMLParser *kmlParser;
+}
+// hello here is some changes
+//there i am inserting some code
+//this code is for second branch
+@end
+
